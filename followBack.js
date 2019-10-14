c4fb710244cfa5d4f746d0c2dffb7c5a61e82269
samuel <ebusameric@gmail.com>
2019-10-14 01:30:42 +0100

    add more descriptive comments

--- followBack.js
+++ followBack.js
@@ -1,5 +1,6 @@
 // This function is used to follow only when the follow button appears on twitter.
-// It contains a loop that always get the first 20 follow button; then called again
+// It contains a loop that always get the first 20 follow button, before you make another call.
+// After the first call, you will have to call the function again to make it work
 
 function followBack(){
     let qSelect = document.querySelectorAll('.css-1dbjc4n.r-1awozwy.r-18u37iz.r-1wtj0ep');
